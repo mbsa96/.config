132fb59b9f63d3b54783101defee88a7a1db574e
mbsa96 <mbsa96@gmail.com>
2025-01-20 11:55:49 -0500

    2025

--- polybar/forest/launch.sh
+++ polybar/forest/launch.sh
@@ -8,7 +8,22 @@ DIR="$HOME/.config/polybar/forest"
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+echo "Esperando que Polybar termine..."
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+notify-send "$(xrandr -q | grep -w connected)"
+
+if xrandr -q | grep -q '^DP-1 connected'; then
+  notify-send "DP-1 conectado. Lanzando Polybar principal..."
+  polybar main -c "$DIR"/config.ini &
+fi
+
+if xrandr -q | grep -q '^HDMI-1 connected'; then
+  notify-send "Lanzando Polybar secundario..."
+  polybar secondary -c "$DIR"/config.ini &
+fi
+
+if xrandr -q | grep -q '^DP-2 connected'; then
+  notify-send "DP-2 conectado. Lanzando Polybar terciario..."
+  polybar terciary -c "$DIR"/config.ini &
+fi
